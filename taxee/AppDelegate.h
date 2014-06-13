5aff807d80c0a41090aeb16366217e42af76c7d5
paperli <paper.li@gmail.com>
2014-06-13 18:04:58 +0800

    Init commit

---
+++ taxee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  taxee
+//
+//  Created by Paper on 6/13/14.
+//  Copyright (c) 2014 Paper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ taxee/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  taxee
+//
+//  Created by Paper on 6/13/14.
+//  Copyright (c) 2014 Paper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
